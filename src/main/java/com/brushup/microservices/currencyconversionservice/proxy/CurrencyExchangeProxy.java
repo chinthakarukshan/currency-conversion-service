56c16658916d354f2c4aab0d2f4fac6b244f51b5
chinthaka <chinthakarukshan@gmail.com>
2022-07-15 00:04:33 +0530

    Added proxy and enabled feign client in the application

---
+++ src/main/java/com/brushup/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.brushup.microservices.currencyconversionservice.proxy;
+
+import com.brushup.microservices.currencyconversionservice.dto.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion getExchangeValue(@PathVariable String from, @PathVariable String to);
+}
